0d849704ced2e7b7a24f25cb0120390d1f8f05b1
Bobby Yang <chrisbobby1217@hotmail.com>
2026-02-27 18:03:49 +0800

    add draw pie

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 from fastmcp import FastMCP
+from fastmcp.utilities.types import Image
+import io
+import matplotlib.pyplot as plt
+
 app = FastMCP()
 
 @app.tool
 def add(n1: int, n2: int) ->int:
     """Add two numbers together."""
+
     return n1 + n2
+
+@app.tool
+def draw_pie_chart(numbers: list[int]) -> Image:
+    """Draw a pie chart from a list of numbers."""
+    plt.pie(numbers)
+    buffer = io.BytesIO()
+    plt.savefig(buffer, format='png')
+    
+    return Image(buffer.getvalue(), format='png')
